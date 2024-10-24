3e3432b4b2551c3046b925369b3a80012cd3a2e5
Sylnus <niharshukla123@gmail.com>
2024-10-24 19:38:32 -0400

    MongoDB initial implementation for sign up and sign in

---
+++ Personal Finance Tracking App/controllers/userController.js
@@ -0,0 +1,26 @@
+const User = require('../models/userModel');
+exports.registerUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const newUser = await User.create({ name, email, password });
+        res.status(201).json(newUser);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+exports.loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).json({ error: 'Invalid email or password' });
+        }
+        const isMatch = await user.comparePassword(password);
+        if (!isMatch) {
+            return res.status(401).json({ error: 'Invalid email or password' });
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
--- Personal Finance Tracking App/server.js
+++ Personal Finance Tracking App/server.js
@@ -1,25 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
+const userController = require('./controllers/userController');
 const app = express();
-const PORT = process.env.PORT || 3000;
+app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'views')));
-app.use(express.static(path.join(__dirname, 'models')));
-app.use(express.static(path.join(__dirname, 'controllers')));
+// mongodb connection
+mongoose.connect('mongodb+srv://nshukla2:itis6112Project@userdata.htivr.mongodb.net/?retryWrites=true&w=majority&appName=userData')
+    .then(() => {
+        console.log('Connected to MongoDB Atlas');
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB Atlas:', error);
+    });
 
+// routes
+app.post('/api/register', userController.registerUser);
+app.post('/api/login', userController.loginUser);
+app.use(express.static('public'));
+app.use(express.static('views'));
+
+//opens homepage when apps run for sign in and sign up
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'homepage.html'));
 });
 
-app.get('/app', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'app.html'));
-});
-
-app.get('/summary', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'summary.html'));
-});
-
+// port
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
